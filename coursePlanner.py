9f6eebacbd1ca5797f8a3ccbc4695abbc7f08f07
graemecrawley <gacrawley@gmail.com>
2017-05-18 02:28:20 -0400

    can now pull data from dropdown menu on the mcmaster site. Can see prereq data, but no structure separating it from other text. Will need to parse text in order to extract the prereq data

--- coursePlanner.py
+++ coursePlanner.py
@@ -15,6 +15,25 @@ for i in range(1,30):
 	x = 0
 	for line in lines:
 		if(line.find('td', width="100%") != None):
-			print line.a.get_text()
-	
+			embeddedHTML = requests.get("http://academiccalendars.romcmaster.ca/"+line.a.get('href')+"&")
+			embeddedTree = html.fromstring(embeddedHTML.content)
+			embeddedRawPage = etree.tostring(embeddedTree)
+			embeddedSoup = BeautifulSoup(embeddedRawPage, 'html.parser')
+			embeddedMainBlock = embeddedSoup.findAll('td', {'class','block_content'})[0]
+			title = embeddedMainBlock.h1
+			print "TITLE:", title.get_text()
+			if(len(embeddedMainBlock.findAll('strong'))!=0):
+				#print embeddedMainBlock.findAll('table')
+				#If it uses a table
+				#print type(embeddedMainBlock.findAll('strong')[0])
+				if(embeddedMainBlock.findAll('strong')[0].get_text()=="Prerequisite(s):"):
+					print embeddedMainBlock.get_text()
+					# if(len(embeddedMainBlock.findAll('table'))!=0):
+					# 	if((embeddedMainBlock.findAll('strong')[0]).findAll('a')!=None):
+					# 		print "PREREQS2:", embeddedMainBlock.findAll('strong')[0].next_sibling.next_sibling
+					# 	print "PREREQS1:", embeddedMainBlock.findAll('strong')[0].next_sibling
+					# #If it does not use a table
+					# else:
+					# 	print "PREREQS2:", embeddedMainBlock.findAll('strong')[0].next_sibling.next_sibling
+		
 
